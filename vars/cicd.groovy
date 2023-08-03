ad68ec84944a9e7859f69f40c46cb181d0ad0f99
obulesu2506 <137786935+obulesu2506@users.noreply.github.com>
2023-08-03 08:03:57 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def tomcatDeploy(jobname,ipaddress,contextpath)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }  
+def runSelenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
